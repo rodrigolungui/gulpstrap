b67bb8eb340403646a1dc2fb383df7d6d1f72566
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2015-01-16 16:20:08 -0200

    Adding task to js compression

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,18 @@ var Constraints = {
   SRC_SCRIPTS: "./app/scripts/*.js",
   BUILD_SCRIPTS: "./app/build/js",
   MAIN_SCRIPT: "main.js",
+  MAIN_SCRIPT_PATH: "./app/build/js/main.js",
 
   /* Vendor scripts */
   VENDOR_SCRIPTS: ['./app/vendor/jquery/dist/jquery.min.js']
 }
 
-
 /* Gulp modules */
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 
 gulp.task('styles', function(){
@@ -33,4 +35,15 @@ gulp.task('scripts', function(){
   gulp.src(Constraints.VENDOR_SCRIPTS.concat(Constraints.SRC_SCRIPTS))
     .pipe(concat(Constraints.MAIN_SCRIPT))
     .pipe(gulp.dest(Constraints.BUILD_SCRIPTS));
+});
+
+gulp.task('compress', function() {
+  gulp.src(Constraints.MAIN_SCRIPT_PATH)
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      if(path.extname === '.js') {
+        path.basename += '.min';
+      }
+    }))
+    .pipe(gulp.dest(Constraints.BUILD_SCRIPTS))
 });
