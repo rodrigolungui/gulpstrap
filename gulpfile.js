a57af8e25adfa86bf51300ed7f59a8c52df584f6
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2015-01-16 16:05:44 -0200

    Adding task for javascript concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,28 @@ var Constraints = {
 
   /* Scripts constraints */
   SRC_SCRIPTS: "./app/scripts/*.js",
-  BUILD_SCRIPTS: "./app/build/js"
+  BUILD_SCRIPTS: "./app/build/js",
+  MAIN_SCRIPT: "main.js",
+
+  /* Vendor scripts */
+  VENDOR_SCRIPTS: ['./app/vendor/jquery/dist/jquery.min.js']
 }
 
 
 /* Gulp modules */
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat');
 
 
 gulp.task('styles', function(){
   gulp.src(Constraints.SRC_STYLES)
     .pipe(sass())
     .pipe(gulp.dest(Constraints.BUILD_CSS));
+});
+
+gulp.task('scripts', function(){
+  gulp.src(Constraints.VENDOR_SCRIPTS.concat(Constraints.SRC_SCRIPTS))
+    .pipe(concat(Constraints.MAIN_SCRIPT))
+    .pipe(gulp.dest(Constraints.BUILD_SCRIPTS));
 });
