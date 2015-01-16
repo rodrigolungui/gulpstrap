ad436b9563600ab456e3a1258650747bd9232f2d
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2015-01-16 15:25:36 -0200

    Initial refactor of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,18 @@
 'use strict';
 
-var gulp 	  = require('gulp'),
-	connect   = require('gulp-connect'),
-	watch	  = require('gulp-watch'),
-	usemin    = require('gulp-usemin'),
-	sass      = require('gulp-sass'),
-	uglify    = require('gulp-uglify'),
-	htmlmin   = require('gulp-minify-html'),
-	jasmine   = require('gulp-jasmine');
+/* Constraints */
 
-gulp.task('server', function() {
+var Constraints = {
+  SRC_STYLES: "./app/styles/*.scss",
+  BUILD_CSS: "./app/build/css"
+}
 
-	connect.server({ livereload: true, root: 'app' });
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-});
 
-gulp.task('styles', function() {
-
-	return gulp.src('app/scss/**/*.scss')
-    	.pipe(sass({ sourceComments: 'map', sourceMap: 'scss', outputStyle: 'compressed' }))
-    	.pipe(gulp.dest('app/css'));
-
-});
-
-gulp.task('test', function () {
-
-	gulp.src('test/**')
-		.pipe(jasmine());
-
-});
-
-gulp.task('default', ['server'], function(){
-
-	gulp.src('app/**', { read: false })
-    	.pipe(watch())
-    	.pipe(connect.reload());
-
-    watch({ glob: 'app/scss/**/*.scss' })
-    	.pipe(sass({ sourceComments: 'map', sourceMap: 'scss', outputStyle: 'compressed' }))
-    	.pipe(gulp.dest('app/css'));
-
-});
-
-gulp.task('build', ['test','styles'], function() {
-
-	var buildName    = ['build', Date.now()].join('/'),
-		buildTasks   = { js: [uglify()], html: [htmlmin()] },
-		buildMessage = ['Build', buildName, 'created.'].join(' ');
-
-	gulp.src('app/*.html')
-		.pipe(usemin(buildTasks))
-		.pipe(gulp.dest(buildName));
-
+gulp.task('styles', function(){
+  gulp.src(Constraints.SRC_STYLES)
+    .pipe(sass())
+    .pipe(gulp.dest(Constraints.BUILD_CSS));
 });
